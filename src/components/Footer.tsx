7e3be173167438fa31bd1cf33d30e9bb45b10d80
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-09-26 15:48:52 +0300

    copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Github, Linkedin, Instagram, Twitter } from 'lucide-react';
 export const Footer: React.FC = () => (
     <footer className="bg-gray-900 text-white py-8">
       <div className="container mx-auto flex justify-between items-center">
-        <div>&copy; 2024 Your Name. All rights reserved.</div>
+        <div>mwanikitiffany&copy; 2024 . All rights reserved.</div>
         <div className="flex space-x-4">
           <a href="#" aria-label="GitHub"><Github /></a>
           <a href="#" aria-label="LinkedIn"><Linkedin /></a>
