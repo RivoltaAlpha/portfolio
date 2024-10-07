86c925ae672496bf7143774468196bad3334f6d5
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-10-07 12:03:56 +0300

    images

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -5,7 +5,7 @@ export const Education: React.FC = () => (
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 justify-center items-center">
         {/* Card for Kirinyaga University */}
         <div className=" bg-gray-800 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition-shadow duration-300">
-        <img src="https://th.bing.com/th/id/OIP.6qKyfSbP2ccA8OOLdh-bCwHaHa?w=166&h=180&c=7&r=0&o=5&pid=1.7"alt= "Kirinyaga University"className="rounded-md mb-4 h-48 w-full object-cover"/>
+        <img src="kyu.jpeg"alt= "Kirinyaga University"className="rounded-md mb-4 h-52 w-full"/>
           <h3 className="text-xl font-semibold mb-4">Kirinyaga University</h3>
           <p className="text-whitw">
             Bachelor's Degree in Computer Science. Focused on software engineering, data structures, and web development.
@@ -14,7 +14,7 @@ export const Education: React.FC = () => (
 
         {/* Card for Teach2give Software Training */}
         <div className=" bg-gray-800 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition-shadow duration-300">
-        <img src="https://th.bing.com/th?id=OIP.YEpxxIGDjjh60yLcyBNlXwAAAA&w=280&h=65&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"alt= "Teach2give"className="rounded-md mb-4 h-48 w-full object-cover"/>
+        <img src="https://th.bing.com/th?id=OIP.YEpxxIGDjjh60yLcyBNlXwAAAA&w=280&h=65&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"alt= "Teach2give"className="rounded-md mb-4 h-52 w-full"/>
           <h3 className="text-xl font-semibold mb-4">Teach2give Software Training</h3>
           <p className="text-whitw">
             Intensive software training program aimed at equipping participants with practical skills in full-stack web development.
