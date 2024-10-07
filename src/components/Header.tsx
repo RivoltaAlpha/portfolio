0e244071477cac494d21d68c8ed842eabd680313
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-10-07 12:04:27 +0300

    styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ export const Header: React.FC = () => (
   <header className="bg-gray-900 text-white p-4">
     <nav className="container mx-auto flex justify-between items-center">
       <div className="text-2xl font-bold">Mwaniki Tifany</div>
-      <div className="flex space-x-6">
-        <a href="#projects" className="text-gray-300 hover:text-white">Projects</a>
-        <a href="#skills" className="text-gray-300 hover:text-white">Skills</a>
-        <a href="#about" className="text-gray-300 hover:text-white">About</a>
-        <a href="#community" className="text-gray-300 hover:text-white">Community</a>
+      <div className="flex space-x-8"> 
+        <a href="#about" className="text-white hover:bg-gray-950 p-2 px-4 rounded">About</a>
+        <a href="#experience" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Experience</a>
+        <a href="#projects" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Projects</a>
+        <a href="#community" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Community</a>
+        <a href="#contact" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Contact me</a>
       </div>
     </nav>
   </header>
