20476840c81bee96063b3de00a04734830c7f22a
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 14:39:56 +0300

    section arrangement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,36 +21,38 @@ export const Hero: React.FC = () => {
   };
 
   return (
-    <section className="py-10 flex mr-[100px] justify-evenly items-center">
-      <div className="container mx-auto mr-[400px] text-center">
-        <h1 className="text-5xl font-bold mb-4">Hey there,</h1>
-        <h2 className="text-4xl font-bold mb-4">
-          I am <span className="text-pink-300">Mwaniki Tifany</span>
-        </h2>
-        <p className="mb-8 text-gray-400">I'm a Fullstack web developer.</p>
-        <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-lg text-sm">
-          My Resume
-        </button>
-      </div>
-      <div className="relative w-[900px] h-[500px] mr-20">
-        <img
-          src={images[currentImage]}
-          alt={`Slide ${currentImage + 1}`}
-          className="w-full h-full object-cover rounded-lg"
-        />
-        <button
-          onClick={prevImage}
-          className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
-        >
-          <ChevronLeft size={24} />
-        </button>
-        <button
-          onClick={nextImage}
-          className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
-        >
-          <ChevronRight size={24} />
-        </button>
-      </div>
-    </section>
+    <div className='mr-[100px] justify-evenly items-center'>
+      <section className="py-10 flex items-center">
+        <div className="container mx-auto mr-[300px] text-center">
+          <h1 className="text-5xl font-bold mb-4">Hey there,</h1>
+          <h2 className="text-8xl font-bold mb-4">
+            I am <span className="text-cards">Mwaniki Tifany</span>
+          </h2>
+          <p className="mb-8 text-gray-400">I'm a Software Engineer and a Data Analyst.</p>
+          <button className="bg-purple-600 hover:bg-purple-300 hover:text-black font-bold py-3 px-6 rounded-lg text-sm">
+            My Resume
+          </button>
+        </div>
+        <div className="relative w-[1000px] h-[500px] mr-20">
+          <img
+            src={images[currentImage]}
+            alt={`Slide ${currentImage + 1}`}
+            className="w-full h-full object-cover rounded-lg"
+          />
+          <button
+            onClick={prevImage}
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+          >
+            <ChevronLeft size={24} />
+          </button>
+          <button
+            onClick={nextImage}
+            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+          >
+            <ChevronRight size={24} />
+          </button>
+        </div>
+      </section>
+    </div>
   );
 };
