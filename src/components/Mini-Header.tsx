4bec949daeaecdf83c9affeed44b28e7f80e6166
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 16:00:13 +0300

    root path

--- src/components/Mini-Header.tsx
+++ src/components/Mini-Header.tsx
@@ -3,7 +3,7 @@ export const MiniHeader: React.FC = () => (
       <nav className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">Mwaniki Tifany</div>
         <div className="flex space-x-6"> 
-          <a href="/home" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Home</a>
+          <a href="/" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Home</a>
           <a href="/skills" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Skills</a>
           <a href="/services" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Services</a>
         </div>
