67b243133b772df72c221cbd499d6150c188bd78
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 14:10:05 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Portfolio from './pages/Home'
+import SkillsPage from './pages/skills'
+import ServicesPage from './pages/services';
 
 function App() {
 
   return (
-    <>
-    <Portfolio />
-    </>
+<Router>
+      <div className="bg-gray-900 text-white p-4">
+        <Routes>
+          <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/home" element={<Portfolio />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
