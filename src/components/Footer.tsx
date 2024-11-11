e11229b33698c6a92c032abef8fe86c630b56810
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 14:09:44 +0300

    links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Github, Linkedin, Instagram, Twitter } from 'lucide-react';
+import { Github, Linkedin, Twitter } from 'lucide-react';
 
 export const Footer: React.FC = () => (
     <footer className="bg-gray-900 text-white py-8">
       <div className="container mx-auto flex justify-between items-center">
         <div>mwanikitiffany&copy; 2024 . All rights reserved.</div>
         <div className="flex space-x-4">
-          <a href="#" aria-label="GitHub"><Github /></a>
-          <a href="#" aria-label="LinkedIn"><Linkedin /></a>
-          <a href="#" aria-label="Instagram"><Instagram /></a>
-          <a href="#" aria-label="Twitter"><Twitter /></a>
+          <a href="https://github.com/RivoltaAlpha" aria-label="GitHub"><Github /></a>
+          <a href="https://www.linkedin.com/in/mwaniki-tifany" aria-label="LinkedIn"><Linkedin /></a>
+          <a href="https://x.com/RivoltaAlpha" aria-label="Twitter"><Twitter /></a>
         </div>
       </div>
     </footer>
