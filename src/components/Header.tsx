86bc89a86de4cc3136a7f089523aa86b41e3c81f
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 14:35:22 +0300

    colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header: React.FC = () => (
   <header className="bg-gray-900 text-white p-4">
     <nav className="container mx-auto flex justify-between items-center">
-      <div className="text-2xl font-bold">Mwaniki Tifany</div>
+      <div className="text-2xl text-teal-500 font-bold">Mwaniki Tifany</div>
       <div className="flex space-x-4"> 
         <a href="#about" className="text-white hover:bg-gray-950 p-2 px-4 rounded">About</a>
         <a href="#experience" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Experience</a>
