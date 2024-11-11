a2dbc3fe2b99cc453d6f8ebf4cf3da0401ac4138
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 14:12:04 +0300

    added routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ export const Header: React.FC = () => (
   <header className="bg-gray-900 text-white p-4">
     <nav className="container mx-auto flex justify-between items-center">
       <div className="text-2xl font-bold">Mwaniki Tifany</div>
-      <div className="flex space-x-8"> 
+      <div className="flex space-x-4"> 
         <a href="#about" className="text-white hover:bg-gray-950 p-2 px-4 rounded">About</a>
         <a href="#experience" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Experience</a>
+        <a href="/skills" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Skills</a>
+        <a href="/services" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Services</a>
         <a href="#projects" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Projects</a>
         <a href="#community" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Community</a>
         <a href="#contact" className="text-white hover:bg-gray-950 p-2 px-4 rounded">Contact me</a>
